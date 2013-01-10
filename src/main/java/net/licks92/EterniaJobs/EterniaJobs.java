96cba71d925e98e9d48dcc620793d05cdc65edf4
Licks92 <licks92@gmail.com>
2013-01-10 23:05:59 +0100

    Create main plugin class

---
+++ src/main/java/net/licks92/EterniaJobs/EterniaJobs.java
@@ -0,0 +1,18 @@
+package net.licks92.EterniaJobs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EterniaJobs extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+}
